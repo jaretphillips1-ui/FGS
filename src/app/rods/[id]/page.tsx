3073910c307d6e8165df1fb37d9e4b38b2fabe8f
Jaret Phillips <lsphillips@hotmail.ca>
2026-02-02 15:24:30 -0400

    FGS: fix rods/[id] params Promise

--- src/app/rods/[id]/page.tsx
+++ src/app/rods/[id]/page.tsx
@@ -1,5 +1,10 @@
-import RodDetailClient from "./RodDetailClient";
+import RodDetailClient from './RodDetailClient'
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <RodDetailClient id={params.id} />;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  return <RodDetailClient id={id} />
 }
